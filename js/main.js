4bef55d1999e51fc9600597b87c7debe9bd218d0
Sthefany Floriano <sthefanyflorianog@gmail.com>
2016-10-30 11:22:04 -0500

    kha

--- js/main.js
+++ js/main.js
@@ -1,18 +1,17 @@
 //CAMBIA EL COLOR DEL NAV AL SITUARSE EN OTRA SECCION
 
-$(document).ready(function(){       
+$(document).ready(function () {
    var scroll_start = 0;
    var startchange = $('#startchange');
    var offset = startchange.offset();
-    if (startchange.length){
-   $(document).scroll(function() { 
-      scroll_start = $(this).scrollTop();
-      if(scroll_start > offset.top) {
-          $(".navbar-default").css('background-color', '#575E65');
-       } else {
-          $('.navbar-default').css('background-color', 'transparent');
-       }
-   });
-    }
+   if (startchange.length) {
+      $(document).scroll(function () {
+         scroll_start = $(this).scrollTop();
+         if (scroll_start > offset.top) {
+            $(".navbar-default").css('background-color', '#575E65');
+         } else {
+            $('.navbar-default').css('background-color', 'transparent');
+         }
+      });
+   }
 });
-   
